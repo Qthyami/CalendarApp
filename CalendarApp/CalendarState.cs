2927dc6bea89c539ff443e2e7d80431268590a5c
Qthyami <Anipabedit@gmail.com>
2026-01-28 05:41:14 +0300

    Add project files.

---
+++ CalendarApp/CalendarState.cs
@@ -0,0 +1,15 @@
+﻿namespace CalendarApp;
+
+public class CalendarState
+{
+    public DateTime SelectedDate { get; private set; } = DateTime.Today;
+    public event Action? OnChange;
+
+    public void
+    SetDate(DateTime date){
+        if (SelectedDate == date)
+            return;
+        SelectedDate = date;
+        OnChange?.Invoke();
+    }
+}
