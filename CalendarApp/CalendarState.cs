815e53ea90dd631d30f4cff08e99de089e28e7c9
Qthyami <Anipabedit@gmail.com>
2026-01-29 04:41:38 +0300

    made DropdownButton FilterButtonsGroup in separated components

--- CalendarApp/CalendarState.cs
+++ CalendarApp/CalendarState.cs
@@ -4,7 +4,7 @@ public class CalendarState
 {
     public DateTime SelectedDate { get; private set; } = DateTime.Today;
     public event Action? OnChange;
-
+    
     public void
     SetDate(DateTime date){
         if (SelectedDate == date)
