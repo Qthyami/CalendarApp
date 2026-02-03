4b357949c34c38975602a73cd02706602d530d34
Qthyami <Anipabedit@gmail.com>
2026-02-03 06:01:29 +0300

    smal changes still working

---
+++ CalendarApp/Common/Css.cs
@@ -0,0 +1,16 @@
+﻿namespace CalendarApp.Common;
+
+public static class Css {
+     public static string 
+    T(this bool value, string @class) {
+        if (value)
+            return @class;
+        return string.Empty;
+    }
+
+    public static string 
+    F(this bool value, string @class) {
+        if (!value) return @class;
+        return string.Empty;
+    }
+}
---
+++ CalendarApp/Common/Strings.cs
@@ -0,0 +1,19 @@
+﻿namespace CalendarApp.Common;
+
+public static class Strings {
+        public static string 
+    AddWordIf(this string @string, bool condition, string word) {
+        if (condition) {
+            return @string.AddWord(word);
+        }
+        return @string;
+    } 
+
+    public static string 
+    AddWord(this string @string, string word) {
+        if (string.IsNullOrEmpty(@string))
+            return word;
+        return $"{@string} {word}";
+    }
+
+}
