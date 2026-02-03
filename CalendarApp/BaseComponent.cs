fa4ccf17200d9febbedf0d0269e2a5ae89567b2f
Qthyami <Anipabedit@gmail.com>
2026-02-03 07:47:45 +0300

    migration on props drill logic, dont working MonthNavigation yet

---
+++ CalendarApp/BaseComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CalendarApp;
+
+public class BaseComponent: ComponentBase {
+    [Parameter] public string Class {get;set;}
+    [Parameter] public string Style {get;set;}
+}
